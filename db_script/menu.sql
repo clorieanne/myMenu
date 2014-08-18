60fa2680f5ba681fb162059b732a464d54539868
clorieanne <clorieanne@gmail.com>
2014-08-18 19:45:20 +0800

    added necessary menu

--- db_script/menu.sql
+++ db_script/menu.sql
@@ -50,3 +50,14 @@ $$
 where menuid = $1;
 $$
   language 'sql'
+
+addmenu(menuid, menuname, menucost) values (1, Pork Sinigang, 315)
+addmenu(menuid, menuname, menucost) values (2, Fish Sinigang, 345)
+addmenu(menuid, menuname, menucost) values (3, Prawn, 395)
+addmenu(menuid, menuname, menucost) values (4, Adobo, 315)
+addmenu(menuid, menuname, menucost) values (5, Tinolang Manok, 315)
+addmenu(menuid, menuname, menucost) values (6, Bulalo Soup, 395)
+addmenu(menuid, menuname, menucost) values (7, Crispy Pat, 595)
+addmenu(menuid, menuname, menucost) values (8, Pancit Canton, 195)
+addmenu(menuid, menuname, menucost) values (9, Bicol Express, 315)
+addmenu(menuid, menuname, menucost) values (10, Bistik Tagalog, 315)
